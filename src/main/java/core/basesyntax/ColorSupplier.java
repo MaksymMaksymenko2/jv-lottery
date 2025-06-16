8e53bb0e8fb3b8c9d1b028dd86bd29d2307e1602
Maksym Maksymenko <Maksimfonmm2@gmail.com>
2025-06-16 15:05:05 +0300

    Fixed code style: added empty lines for checkstyle compliance

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -1,6 +1,9 @@
 package core.basesyntax;
+
 import java.util.Random;
+
 public class ColorSupplier {
+
     private static final Random random = new Random();
 
     public static Color getRandomColor() {
